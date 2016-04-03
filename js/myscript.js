e7e863bb56c708a4f8344b3449d8660d972fd9b4
craby-c <securicula@gmail.com>
2016-04-04 06:44:19 +0700

    major fix

--- js/myscript.js
+++ js/myscript.js
@@ -4,6 +4,29 @@
 $(document).ready(function(){
     $(".article-head").click(function(){
         $(".clip-text").css("height", "auto")
+    });
+
+    //переключалка карт
+    $("#rostov").click(function(){
+       $("#kr-address").fadeOut(function(){
+           $("#rd-address").fadeIn();
+       });
+    });
+
+    $("#krdr").click(function(){
+        $("#rd-address").fadeOut(function(){
+            $("#kr-address").fadeIn();
+        });
+    });
+
+    //красная палочка и серый шрифт в faq
+    var line = $("<div></div>").addClass("line");
+    var title = $(".panel-group>.panel>.panel-heading>.panel-title");
+    $(title).before(line);
+    $(title).click(function(){
+        $(this).prev().toggle();
+        $(title).not(this).prev().css("display", "none");
+        $(title).not(this).toggleClass("gray-text");
     })
 });
 
@@ -21,30 +44,54 @@ $(".right").click(function(){
 $('.collapse').collapse();
 
 ymaps.ready(init);
-var myMap,
-    myPlacemark;
+
+var Map1;
+var Map2;
+var Placemark1;
+var Placemark2;
 
 function init(){
-    myMap = new ymaps.Map("map", {
+    Map1 = new ymaps.Map("map1", {
         center: [45.059536, 38.962692],
         zoom: 16
     });
 
-    myPlacemark = new ymaps.Placemark([45.059536, 38.962692], {
-        hintContent: '"Хорс-инвест"',
-        balloonContent: ''
-    }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: '/new-horse/images/флажок.png',
-        // Размеры метки.
-        iconImageSize: [150, 110],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-30, -110]
+    Map2 = new ymaps.Map("map2", {
+        center: [47.233809, 39.715624],
+        zoom: 16
     });
 
-    myMap.geoObjects.add(myPlacemark);
+    Placemark1 = new ymaps.Placemark([45.059536, 38.962692], {
+            hintContent: 'г.Краснодар, ул.Монтажников 1/4, оф.1103 (11 этаж)',
+            balloonContent: 'г.Краснодар, ул.Монтажников 1/4, оф.1103 (11 этаж), 8 (800) 234-81-88, 8 (861) 279-40-38'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/флажок.png',
+            // Размеры метки.
+            iconImageSize: [150, 110],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-30, -110]
+        });
+    Placemark2 = new ymaps.Placemark([47.233809, 39.715624],{
+            hintContent: 'г.Ростов на Дону, ул.Варфоломеева 259-261, оф.409',
+            balloonContent: 'г.Ростов на Дону, ул.Варфоломеева 259-261, оф.409, тел: 8 (863) 218-87-84'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/флажок.png',
+            // Размеры метки.
+            iconImageSize: [150, 110],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-30, -110]
+        });
+
+    Map1.geoObjects.add(Placemark1);
+    Map2.geoObjects.add(Placemark2);
 }
