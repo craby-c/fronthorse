4ec4e8c55b6bd9e730d9fe072efe9a715aea4fbb
pinballwizard <menstenebris@gmail.com>
2016-06-12 17:56:46 +0700

    add top trade popover

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -2,8 +2,36 @@
  * Created by vera on 31.05.16.
  */
 
+// фунция для изменения стилей полей, прошедших валидизацию (addClass "valid" or "invalid")
+// добавить текст "Проверте правильность заполнения поля" или
+// иконку <span class="glyphicon glyphicon-ok" aria-hidden=true></span> с цветом #cdcdcd
+
+
 $(document).ready(function(){
-//    фунция для изменения стилей полей, прошедших валидизацию (addClass "valid" or "invalid")
-//    добавить текст "Проверте правильность заполнения поля" или
-//    иконку <span class="glyphicon glyphicon-ok" aria-hidden=true></span> с цветом #cdcdcd
+    var hash = window.location.hash;
+    if (hash != ''){
+        $('a[href='+hash+']').tab('show');
+    }
+    var tb = $(".trade-block");
+    tb.children(".doc-link").popover({
+        trigger: "manual",
+        placement: "bottom",
+        html: true,
+        content: '<div class="top-trade text-center">' +
+        '<p>1st place<br><span class="red-link">%%%</span></p>' +
+        '<p>2nd place<br><span class="red-link">%%%</span></p>' +
+        '<p>3rd place<br><span class="red-link">%%%</span></p></div>'
+    });
+    var background;
+    tb.hover(
+        function () {
+            background = $(this).children(".medal").css("background");
+            $(this).children(".medal").css("background", "None");
+            $(this).children(".doc-link").popover('show');
+        },
+        function() {
+            $(this).children(".medal").css("background", background);
+            $(this).children(".doc-link").popover('hide')
+        }
+    );
 });
