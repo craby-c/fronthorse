dc46f3f73244b8d7bb4e012697fee97f9eeaba14
pinballwizard <menstenebris@gmail.com>
2016-06-12 21:03:17 +0700

    client form progress bar

--- dashboard/js/script.js
+++ dashboard/js/script.js
@@ -8,10 +8,12 @@
 
 
 $(document).ready(function(){
+    // работа bookmarks при переходе
     var hash = window.location.hash;
     if (hash != ''){
         $('a[href='+hash+']').tab('show');
     }
+    // popover для лучших трейдеров
     var tb = $(".trade-block");
     tb.children(".doc-link").popover({
         trigger: "manual",
@@ -34,4 +36,46 @@ $(document).ready(function(){
             $(this).children(".doc-link").popover('hide')
         }
     );
+
+    // Переключение формы добавления клиента
+    var progress_bar = $(".progress-bar");
+    var steps_text = $(".steps").children("i");
+    var client_form = $("#client_form");
+    var max_step = client_form.children(".form-group").length;
+    var step = 1;
+    var max_value = 100;
+    var value_step = max_value/max_step;
+    var value = value_step;
+    progress_bar.css("width", value+"%");
+    $(".backward").css("visibility", "hidden");
+    client_form.children(".form-group").css("display","none");
+    client_form.children(".form-group").eq(step-1).css("display","block");
+    $(".forward").click(function () {
+        if (step < max_step){
+            step += 1;
+            value += value_step;
+            if (step >= max_step){
+                $(this).css("visibility", "hidden");
+            }
+            steps_text.text(step + " шаг");
+            $(".backward").css("visibility", "visible");
+            progress_bar.css("width", value+"%");
+            client_form.children(".form-group").css("display","none");
+            client_form.children(".form-group").eq(step-1).css("display", "block");
+        }
+    });
+    $(".backward").click(function () {
+        if (step > 1){
+            step -= 1;
+            value -= value_step;
+            if (step <= 1){
+                $(this).css("visibility", "hidden");
+            }
+            $(".forward").css("visibility", "visible");
+            steps_text.text(step + " шаг");
+            progress_bar.css("width", value+"%");
+            client_form.children(".form-group").css("display","none");
+            client_form.children(".form-group").eq(step-1).css("display", "block");
+        }
+    });
 });
