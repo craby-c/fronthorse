b8300962f6c2a54b0f7a1d2f8586d7a4788f2b55
craby-c <securicula@gmail.com>
2016-06-12 12:30:07 +0700

    add dashboard

---
+++ dashboard/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by vera on 31.05.16.
+ */
+
+$(document).ready(function(){
+//    фунция для изменения стилей полей, прошедших валидизацию (addClass "valid" or "invalid")
+//    добавить текст "Проверте правильность заполнения поля" или
+//    иконку <span class="glyphicon glyphicon-ok" aria-hidden=true></span> с цветом #cdcdcd
+});
