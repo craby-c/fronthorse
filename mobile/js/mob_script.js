3ad20549f70cf90f3879b42afa5eb8b46072cc53
craby-c <securicula@gmail.com>
2016-06-26 14:38:12 +0700

    background for navbar

--- mobile/js/mob_script.js
+++ mobile/js/mob_script.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function(){
+    var tr_white = 'rgba(255,255,255,0.9)';
+    $('.navbar-toggle').on('click', first_click);
+    function first_click (){
+        $('.navbar-toggle').css('transform', 'rotate(90deg)');
+        $('.navbar-header').css('background', tr_white);
+        $('#bs-collapse').css('background', tr_white).collapse("show");
+        $(this).off('click', first_click).on('click', second_click);
+    }
+    function second_click (){
+        $('.navbar-toggle').css('transform', 'none');
+        $('.navbar-header').css('background', 'transparent');
+        $('#bs-collapse').css('background', 'transparent').collapse("hide");
+        $(this).off('click', second_click).on('click', first_click);
+    }
+});
